d8d8e1c9b78e7adfcb6db35a2c8020de1c801cc4
zaccomer40 <38451962+zaccomer40@users.noreply.github.com>
2022-10-07 14:19:03 -0400

    weather update

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '97fc30933d7294695aedab00bef5be46', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
+	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '37.208',
+	defaultLongitude: '-80.395',
 
 	// Autochange
 	autoChangeTheme: true,
